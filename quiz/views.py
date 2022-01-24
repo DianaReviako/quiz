18385399ac1ae47569a55d2e8e166e03073b0baa
DianaReviako <dianarevako8@gmail.com>
2022-01-24 16:36:58 +0300

    partially implemented the functionality of displaying data from the database

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from.models import Topic, Question
 
-# Create your views here.
+
+class HomePageView(TemplateView):
+
+    template_name = "main_quiz.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['topics'] = Topic.objects.all()
+        return context
